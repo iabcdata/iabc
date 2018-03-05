951d70243f03db3b4bf5bb74324969928b4a1126
shuchen <h@heyx.net>
2018-03-05 10:38:32 +0800

    shuchen : 注释

--- iabc-dubbo/demoa/demoa-share/src/main/java/io/iabc/dubbo/demoa/share/dto/CookiesDTO.java
+++ iabc-dubbo/demoa/demoa-share/src/main/java/io/iabc/dubbo/demoa/share/dto/CookiesDTO.java
@@ -1,17 +1,3 @@
-/*
- * Project: iabc
- *
- * File Created at 2018-03-05
- *
- * Copyright 2012-2015 Greenline.com Corporation Limited.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Greenline Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Greenline.com.
- */
 package io.iabc.dubbo.demoa.share.dto;
 
 import com.alibaba.fastjson.JSON;
